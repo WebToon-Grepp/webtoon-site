d030849d96716ab8241b275c8ffc675471c921ea
Rooam Lee <leewr9@naver.com>
2025-03-07 09:57:53 +0900

    site:: django -> flask 기반 변경

--- app/views.py
+++
@@ -1,9 +0,0 @@
-from django.utils import timezone
-from django.shortcuts import render
-
-# Create your views here.
-def main(request):
-    context = {
-        'timestamp': timezone.now()
-    }
-    return render(request, 'main.html', context)
